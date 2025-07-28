e8c398b768a841d571227291fdbb1fcd7ad2f37a
Mugunthan <mugunth19@gmail.com>
2025-07-28 14:16:52 +0000

    Fixes

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -1,12 +1,11 @@
-
-resource "aws_iam_role" "eks_role" {
-  name = "eksClusterRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = { Service = "eks.amazonaws.com" }
-    }]
-  })
-}
+#resource "aws_iam_role" "eks_role" {
+#  name = "eksClusterRole"
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17",
+#    Statement = [{
+#      Action = "sts:AssumeRole",
+#      Effect = "Allow",
+#      Principal = { Service = "eks.amazonaws.com" }
+#    }]
+#  })
+#}
